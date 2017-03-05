b5c8dca4e0981e9cacffa8d274734b7869b01b39
Yasir <memyselfandyasir@hotmail.com>
2017-03-05 17:29:28 +0500

    now getting the starting point from the genreated reads.

--- EulerianDNA/debruijnEulerianDNA.py
+++ EulerianDNA/debruijnEulerianDNA.py
@@ -1,14 +1,15 @@
 import random
 
 
-def find_eulerian_tour(graph, d, tour):
-    edge_val = list(graph.keys())[0]
+def find_eulerian_tour(graph, d, tour, starting_point=None):
+    edge_val = starting_point or list(graph.keys())[0]
     # creating a new tour
     tour.append([])
     tour_number = len(tour) - 1
     while graph.get(edge_val):
-        node_val = graph[edge_val][0]
-        graph[edge_val].pop(0)
+        index_to_use = 0 # len(graph[edge_val]) - 1
+        node_val = graph[edge_val][index_to_use]
+        graph[edge_val].pop(index_to_use)
         tour[tour_number].append(node_val)
         # checking the node is empty
         if not graph.get(edge_val):
@@ -44,7 +45,6 @@ def split_reads(read, d):
     prev_c = 0
     c = 0
     reads = []
-    # adding one to make sure the last read also gets selected
     for c in range(d, len(read), d):
         # ToDo: Make generic logic for pairs
         first = read[prev_c: c]
@@ -74,50 +74,91 @@ def generate_graph_from_reads(splitted_reads):
 
 
 def pretty_print_tour(eu_tours):
+    str = ''
     for tour in eu_tours:
+        for point in tour:
+            str += point[:1]
         print(' -> '.join(tour))
+    # appending the last part of the tour
+    str += tour[len(tour)-1][1:]
+    print('String formed: {}'.format(str))
+    return str
 
 
 def merge_eu_path(eu_tours):
-    final_eu = []
     if len(eu_tours) > 1:
         for tour in eu_tours:
             # first part matches the last part of any tour
             part_to_match = tour[0][:2]
             parts_could_merge = [t for t in eu_tours if t[len(t) - 1][1:] == part_to_match]
-            if parts_could_merge:
-                final_eu.append(parts_could_merge[0] + tour)
+            # should not be getting merged with self
+            if parts_could_merge and parts_could_merge[0] != tour:
+                # add the merged part
+                eu_tours.append(parts_could_merge[0] + tour)
+                # and remove the old objects
+                # if parts_could_merge[0] in eu_tours:
                 eu_tours.pop(eu_tours.index(parts_could_merge[0]))
-            else:
-                final_eu.append(tour)
+                # if tour in eu_tours:
+                eu_tours.pop(eu_tours.index(tour))
     else:
         return eu_tours
 
-    return final_eu
+    return eu_tours
 
+
+def find_starting_point_in_reads(splitted_reads):
+    all_nodes = {}
+    for read in splitted_reads:
+        # maintaining the ins and outs count to get the node
+        # for all the outs, subtract 1 from the count
+        if not all_nodes.get(read[1:]):
+            all_nodes[read[1:]] = -1
+        else:
+            all_nodes[read[1:]] += -1
+        # for all the ins, add 1 in the count
+        if not all_nodes.get(read[:2]):
+            all_nodes[read[:2]] = 1
+        else:
+            all_nodes[read[:2]] += 1
+
+    for starting in all_nodes:
+        if all_nodes.get(starting) > 0:
+            return starting
+    # no node found that have uneven count
+    return None
 
 if __name__ == "__main__":
     # the read splitting currently works for split size, logic can be improved to handle generalized split size
     read_split_size = 3
     # length of n character string, randomly generated, multiple of split size preferably
-    DNA_read = generate_random_read(read_split_size * 5)
+    DNA_read = generate_random_read((read_split_size *6) - 1)
 
     # with random string, the result is not exact match, as there are multiple collusion paths
     # DNA_read = 'ACGTACGT'
     # DNA_read = 'TAATGCCATGGGATGTAT'
-    # DNA_read = 'CCAGTATTGCACGTA'
+    # DNA_read = 'GCCGACCAACACGCC'
+    # DNA_read = 'TTTCAGCAGTAGAA'
     print(DNA_read)
     # this will break the string into format
     splitted_reads = split_reads(DNA_read, read_split_size)
     # print splitted_reads.pop(5)
-    print(splitted_reads)
+    print('Splitted Reads: \n {}'.format(splitted_reads))
+    starting_point = find_starting_point_in_reads(splitted_reads)
+    print('Got starting point: {}'.format(starting_point))
     graph = generate_graph_from_reads(splitted_reads)
-    print(graph)
-    eu_tour = find_eulerian_tour(graph, read_split_size, [])
+    print('Gerneated graph: {}'.format(graph))
+    eu_tour = find_eulerian_tour(graph, read_split_size, [], starting_point)
     print('Without merging path:')
     pretty_print_tour(eu_tour)
-    eu_tour = merge_eu_path(eu_tour)
+    index = 1
+    #  mergeable parts will be merged
+    merge_eu_path(eu_tour)
     print('After merging path:')
-    pretty_print_tour(eu_tour)
+    str = pretty_print_tour(eu_tour)
+    if DNA_read == str:
+        print('exact matching path found')
+    else:
+        print('no exact matching path found')
+
     # should always find a path as the read was generated from a connected string...
     # print(find_eulerian_tour(graph))
